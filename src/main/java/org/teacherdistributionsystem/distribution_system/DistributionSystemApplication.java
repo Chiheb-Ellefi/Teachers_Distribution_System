b8d72584800502521ef7f319b7b840b70aab4a8f
chihebellefi <chihebellefi888@gmail.com>
2025-10-11 10:33:39 +0100

    first commit

---
+++ src/main/java/org/teacherdistributionsystem/distribution_system/DistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package org.teacherdistributionsystem.distribution_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/teacherdistributionsystem/distribution_system/DistributionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teacherdistributionsystem.distribution_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
